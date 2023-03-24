ca39609d5c515b822f13481d1da95275eda2f305
Skylark92 <skylark92@live.co.kr>
2023-03-24 16:24:50 +0900

    [Modify] Router 내에 헤더 및 메인 컨테이너 추가

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,14 +1,19 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import Main from "../pages/Main/Main";
 import Wiki from "../pages/Wiki/Wiki";
 
 export default function Router() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/wiki/:id" element={<Wiki />} />
-      </Routes>
+      <header className="app-header">
+        <h1 className="app-logo"><Link to="/">위키위키</Link></h1>
+      </header>
+      <main className="app-main-container">
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/wiki/:id" element={<Wiki />} />
+        </Routes>
+      </main>
     </BrowserRouter>
   )
 }
